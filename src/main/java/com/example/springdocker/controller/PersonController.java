1266450080ff52e5876b913ef119ff74aab66905
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-05-11 12:06:32 +0200

    Clean code PersonController.java

--- src/main/java/com/example/springdocker/controller/PersonController.java
+++ src/main/java/com/example/springdocker/controller/PersonController.java
@@ -1,8 +1,6 @@
 package com.example.springdocker.controller;
 
-import com.example.springdocker.model.Food;
 import com.example.springdocker.model.Person;
-import com.example.springdocker.service.FoodService;
 import com.example.springdocker.service.PersonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
