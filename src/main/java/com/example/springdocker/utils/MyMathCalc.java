53bf260f0586308ac3b0b0da4a5528ba8f84ee0b
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-05-13 11:53:52 +0200

    add a MyMathCalc.java and some methods

---
+++ src/main/java/com/example/springdocker/utils/MyMathCalc.java
@@ -0,0 +1,20 @@
+package com.example.springdocker.utils;
+
+/**
+ * Created by Ashkan Amiri
+ * Date:  13/05/2021
+ * Time:  11:46
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ */
+public class MyMathCalc{
+    public int add(int a , int b){
+        return a + b;
+    }
+    public int multiply(int a , int b){
+        return a * b;
+    }
+    public int divide(int a , int b){
+        return a / b;
+    }
+}
